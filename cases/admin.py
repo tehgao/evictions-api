96e080e6c855debd149689fbd5bb6f4489854c24
Alvin Gao <gaovinal@gmail.com>
2020-01-18 03:35:23 +0000

    whoops forgot to correct case.admin to account for change in models

--- cases/admin.py
+++ cases/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from cases.models import Address, Party, Person, Attorney, Company, Case, Event
+from cases.models import Address, Party, Attorney, Case, Event
 
-case_models = [Address, Party, Person,
-               Attorney, Company, Case, Event]
+case_models = [Address, Party, Attorney, Case, Event]
 admin.site.register(case_models)
